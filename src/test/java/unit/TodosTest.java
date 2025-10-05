c374a87b9c74a283717f7271de95f63ce0d71a17
Mona <mona.kalaoun@mail.mcgill.ca>
2025-10-05 16:12:17 -0400

    ignore

--- src/test/java/unit/TodosTest.java
+++ src/test/java/unit/TodosTest.java
@@ -5,12 +5,21 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.*;
+import java.util.Map;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @TestMethodOrder(MethodOrderer.Random.class)
 public class TodosTest {
 
     private static final String BASE_URL = "http://localhost:4567/todos";
     private static HttpClient client = HttpClient.newHttpClient();
+    ObjectMapper objectMapper = new ObjectMapper();
+    
+    Map<String, Object> projectTodo = Map.of(
+        "title", "Project Todo",
+        "doneStatus", false,
+        "description", "This is a todo for the project"
+    );
 
     // --------------------- /todos ----------------------
     @Test
@@ -27,7 +36,19 @@ public class TodosTest {
 
     @Test
     public void post_todos_201() throws IOException, InterruptedException { 
-        String json = "{\"title\":\"New Todo\",\"doneStatus\":false}";
+        String jsonBody = objectMapper.writeValueAsString(projectTodo);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL))
+                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+        assertEquals(201, response.statusCode());
+    }
+
+    @Test
+    public void post_todos_202() throws IOException, InterruptedException { 
+        String json = "{\"title\":\"Another Todo\",\"doneStatus\":false}";
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(BASE_URL))
                 .POST(HttpRequest.BodyPublishers.ofString(json))
